9dd4d36083edf927afefb8cec4fd067bc1ff48d2
gamer0kayf1n3 <muser0943@gmail.com>
2025-02-15 21:58:52 +0800

    new

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
-
+from flask_socketio import SocketIO, join_room, leave_room, send, emit
+import requests
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 @app.route('/')
 def home():
     return render_template('about')
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', to=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', to=room)
+
+@socketio.on('connect')
+def test_connect(auth):
+    
+    emit('global', "Connected (server)")
+
+@socketio.on('disconnect')
+def test_disconnect(reason):
+    print('Client disconnected, reason:', reason)
 
 if __name__ == '__main__':
     app.run(debug=True)
